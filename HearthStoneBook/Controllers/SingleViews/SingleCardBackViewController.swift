e9d2253d142ee9e22bb0b484fac2637a46a91fec
Daniil <rodindaniil@gmail.com>
2019-05-20 17:38:12 +0400

    Так же комменты

--- HearthStoneBook/Controllers/SingleViews/SingleCardBackViewController.swift
+++ HearthStoneBook/Controllers/SingleViews/SingleCardBackViewController.swift
@@ -37,15 +37,4 @@ class SingleCardBackViewController: UIViewController {
             displayCardBackImages(backImg: image, name: name, backImgView: backImageView, nameLabel: nameLabel)
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
